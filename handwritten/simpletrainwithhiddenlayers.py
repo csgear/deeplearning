81f34fc1bb2b11ffce41c85871858c2520bff586
csgear <csgear@live.com>
2020-01-10 18:42:25 -0500

    add simple mnist cases

---
+++ handwritten/simpletrainwithhiddenlayers.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+EPOCHS = 200
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2
+
+mnist = keras.datasets.mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+
+RESHAPED = 784
+
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train /= 255
+X_test /= 255
+
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+Y_train = tf.keras.utils.to_categorical(Y_train, NB_CLASSES)
+Y_test = tf.keras.utils.to_categorical(Y_test, NB_CLASSES)
+
+# build model
+
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.Dense(N_HIDDEN, input_shape=(RESHAPED, ), name='dense_layer_1', activation='relu'))
+model.add(tf.keras.layers.Dense(N_HIDDEN, name='dense_layer_2', activation='relu'))
+model.add(tf.keras.layers.Dense(NB_CLASSES, name="dense_layer_3", activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='SGD', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
+test_loss, test_acc = model.evaluate(X_test, Y_test)
+
+print('Test accuracy: ', test_acc)
